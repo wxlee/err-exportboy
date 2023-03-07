75c98fd716d638f3d27377c653da74d491870125
Walker <walker1202@gmail.com>
2023-03-07 13:56:41 +0800

    use request, not curl

--- exportboy.py
+++ exportboy.py
@@ -2,17 +2,16 @@ from errbot import BotPlugin, botcmd
 import subprocess
 import time
 import os
+import requests
 
 user_token = os.environ.get('JENKINS_USER_TOKEN')
 prj_token = os.environ.get('JENKINS_PRJ_TOKEN')
 url = os.environ.get('JENKINS_URL')
 
-exportboy_upload_url = 'http://' + user_token + '@' + url + '/view/exportboy/job/1_exportboy_create_upload_link_v2/buildWithParameters?token=' + prj_token + '&sequence_num='
-exportboy_upload_last_consoleText = 'http://' + user_token + '@' + url + '/view/exportboy/job/1_exportboy_create_upload_link_v2/lastBuild/consoleText'
-
-exportboy_download_url = 'http://' + user_token + '@' + url + '/view/exportboy/job/3_exportboy_create_download_link_v2/buildWithParameters?token=' + prj_token + '&sequence_num='
-exportboy_download_last_consoleText = 'http://' + user_token + '@' + url + '/view/exportboy/job/3_exportboy_create_download_link_v2/lastBuild/consoleText'
-
+exportboy_upload_url = f"http://{user_token}@{url}/view/exportboy/job/1_exportboy_create_upload_link_v2/buildWithParameters?token={prj_token}&sequence_num="
+exportboy_upload_last_consoleText = f"http://{user_token}@{url}/view/exportboy/job/1_exportboy_create_upload_link_v2/lastBuild/consoleText"
+exportboy_download_url = f"http://{user_token}@{url}/view/exportboy/job/3_exportboy_create_download_link_v2/buildWithParameters?token={prj_token}&sequence_num="
+exportboy_download_last_consoleText = f"http://{user_token}@{url}/view/exportboy/job/3_exportboy_create_download_link_v2/lastBuild/consoleText"
 
 class Jenkins(BotPlugin):
     @botcmd()
@@ -39,22 +38,29 @@ class Jenkins(BotPlugin):
 
     @botcmd(split_args_with=None)
     def exportboy_upload(self, msg, args):
-        full_url = exportboy_upload_url + args[0]
-        p = subprocess.Popen(["curl", full_url ], stdout=subprocess.PIPE, text=True)
-        p.wait(5)
+        number = args[0]
+        full_url = f"{exportboy_upload_url}{number}"
+        response = requests.get(full_url)
         yield "Now working for upload link, please wait..."
+
+        # the consoleText will delay several seconds
         time.sleep(10)
-        p1 = subprocess.Popen(["curl", exportboy_upload_last_consoleText ], stdout=subprocess.PIPE, universal_newlines=True, text=True)
-        output, err = p1.communicate()
+
+        response = requests.get(exportboy_upload_last_consoleText)
+        output = response.text
         yield output
 
     @botcmd(split_args_with=None)
     def exportboy_download(self, msg, args):
-        full_url = exportboy_download_url + args[0]
-        p = subprocess.Popen(["curl", full_url ], stdout=subprocess.PIPE, text=True)
-        p.wait(5)
+        number = args[0]
+        full_url = f"{exportboy_download_url}{number}"
+        response = requests.get(full_url)
         yield "Now working for download link, please wait..."
+
+        # the consoleText will delay several seconds
         time.sleep(10)
-        p1 = subprocess.Popen(["curl", exportboy_download_last_consoleText ], stdout=subprocess.PIPE, universal_newlines=True, text=True)
-        output, err = p1.communicate()
+
+        response = requests.get(exportboy_download_last_consoleText)
+        output = response.text
         yield output
+
